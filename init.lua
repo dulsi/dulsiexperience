2b917fe8309dc1000676b75449c15745e79f6ca8
Dennis Payne <dulsi@identicalsoftware.com>
2015-08-25 17:16:15 -0400

    Adding snowjack texture. Snowjack is created instead of snowman by placing the jack o lantern on the snow blocks.

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+dofile(minetest.get_modpath("qt_mobs").."/api.lua")
+
 minetest.register_node(":qt_dungeons:jackolantern", {
 	description = "Jack'o'lantern",
 	tiles = {"pumpkin_top.png", "pumpkin_bottom.png", "pumpkin_side.png",
@@ -30,7 +32,7 @@ minetest.register_node(":qt_dungeons:jackolantern", {
     minetest.remove_node({x=pos.x,y=pos.y-1,z=pos.z})
     local p = pointed_thing.above
     p.y = p.y+1
-    minetest.env:add_entity({x=pos.x,y=pos.y-1,z=pos.z}, "qt_mobs:snowman")
+    minetest.env:add_entity({x=pos.x,y=pos.y-1,z=pos.z}, "dulsiexperience:snowjack")
    else
     local ret = minetest.item_place(stack, placer, pointed_thing)
     if ret==nil then
@@ -40,4 +42,43 @@ minetest.register_node(":qt_dungeons:jackolantern", {
     end
    end
   end,
+})
+
+qt_mobs:register_mob("dulsiexperience:snowjack", {
+	type = "animal",
+	hp_max = 15,
+	collisionbox = {-0.4, -0.01, -0.4, 0.4, 2, 0.4},
+	textures = {"snowjack.png"},
+	visual = "mesh",
+	mesh = "snowman.x",
+	visual_size = {x=6, y=6},
+	makes_footstep_sound = true,
+	walk_velocity = 1,
+	armor = 100,
+	drops = {
+		{name = "default:snow",
+		chance = 1,
+		min = 2,
+		max = 3,},
+		{name = "qt_mobs:soul",
+		chance = 2,
+		min = 1,
+		max = 1,},
+	},
+	drawtype = "front",
+	water_damage = 10,
+	lava_damage = 50,
+	light_damage = 0,
+	animation = {
+		speed_normal = 0,
+		stand_start = 0,
+		stand_end = 0,
+		walk_start = 0,
+		walk_end = 0,
+	},
+	view_range = 5,
+	sounds = {
+		--random = "sheep",
+	},
+	on_rightclick = nil,
 })
